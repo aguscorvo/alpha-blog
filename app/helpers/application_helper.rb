c71b30a5e2b017486b4b238e218117143d6b80de
aguscorvo <agustina@streaver.com>
2021-09-17 08:46:41 -0300

    Authentication helper methods: current_user and logged_in?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,13 @@ module ApplicationHelper
     size = options[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
-  end 
+  end
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
